ea0b50417a4eed466d50962959f374cca57c092c
Saba <saba.sadegh67@gmail.com>
2022-12-10 14:54:19 +0330

    add functionality to toggle btn

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+//etting date dynamic
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+//nav toggle part
+const navToggle = document.querySelector(".nav-toggle");
+const linkContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linkContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linkContainer.style.height = `${linksHeight}px`;
+  } else {
+    linkContainer.style.height = 0;
+  }
+});
